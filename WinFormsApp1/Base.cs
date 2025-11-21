a2a29714362f382a12e5e5fd11059077c5c17ce9
Yassine <bracosse1@gmail.com>
2025-11-21 02:18:25 +0100

    Add project files.

---
+++ WinFormsApp1/Base.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class Base : MetroFramework.Forms.MetroForm
+    {
+        public Base()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
